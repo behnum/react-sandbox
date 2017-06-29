90f82027924adeb43c3f6dce26afb33c438e3a22
Behnam Baharmand <bbaharmand@gmail.com>
2017-06-29 12:52:33 +0430

    Basic component combination

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React, { Component } from 'react';
+// TODO: Move const products outside of ProductList (based on source)
 
-class BaseComponent extends Component {
+import React, { Component } from "react";
+// Based on: https://goo.gl/2ZE1mX
+class ProductList extends Component {
   render() {
+    const products = [
+      {
+        name: "Banana",
+        price: 123
+      },
+      {
+        name: "Oil",
+        price: 333
+      }
+    ];
+
+    const productList = products.map(function(product, index) {
+      return (
+        <ProductItem key={index} name={product.name} price={product.price} />
+      );
+    });
+
     return (
       <div>
-        Hola!
+        <h1>Product List</h1>
+        {productList}
       </div>
     );
   }
 }
 
-export default BaseComponent;
+class ProductItem extends Component {
+  render() {
+    return (
+      <div>
+        <h2>PRODUCT</h2>
+        <p>
+          Name : {this.props.name}
+        </p>
+        <p>
+          Price: {this.props.price}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
