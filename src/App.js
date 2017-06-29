2671101e67963bcd84e4f7b0f990fe660cfe175b
Behnam Baharmand <bbaharmand@gmail.com>
2017-06-29 10:07:51 +0430

    Base Ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BaseComponent extends Component {
+  render() {
+    return (
+      <div>
+        Hola!
+      </div>
+    );
+  }
+}
+
+export default BaseComponent;
