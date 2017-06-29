0461da9b0c9a743095cedf1056e6e7915042ae59
Behnam Baharmand <bbaharmand@gmail.com>
2017-06-29 10:31:49 +0430

    Add Button Enabled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class BaseComponent extends Component {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      numChildren: 0
+    };
+  }
+
+  render() {
+    const children = [];
+
+    for (var i = 0; i < this.state.numChildren; i++) {
+      children.push(<Child number={i} />);
+    }
+
+    return (
+      <Parent addClick={this.onAddClick.bind(this)}>
+        {children}
+      </Parent>
+    );
+  }
+
+  onAddClick() {
+    this.setState({
+      numChildren: this.state.numChildren + 1
+    });
+  }
+}
+
+class Parent extends Component {
   render() {
     return (
       <div>
-        Hola!
+        <button onClick={this.props.addClick}>Add a child</button>
+
+        {this.props.children}
       </div>
     );
   }
 }
 
-export default BaseComponent;
+class Child extends Component {
+  render() {
+    return <div>Child</div>;
+  }
+}
+
+export default App;
